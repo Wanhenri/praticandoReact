84b9e93723ab604616e2b2f9ad9bad130d5f0cb8
Wanhenri <you@example.com>
2020-04-08 13:04:55 -0300

    [fix](frontend):modificação nos botoes e acrescimo de hora

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
 
+function Button(props) {
+  const handleClick = () => props.onClickFunction(props.increment);
+  return (
+    <button onClick={handleClick}>
+      +{props.increment}
+    </button>
+  );
+}
+
+function Display (props) {
+  return (
+    <div>{props.message}</div>
+  );
+}
+
+function HourNow () {
+  return(
+  <div>
+    Hello HTML
+    <input />
+    <pre>{(new Date).toLocaleTimeString()}</pre>
+  </div>
+  );
+}
 
 function App() {
-  const[ counter, setCounter ] = useState(0) 
+  const [ counter, setCounter ] = useState(0)
+  const incrementCounter = (incrementValue) => setCounter(counter + incrementValue); 
   return (
-  <button onClick={() => setCounter(counter +1)}>{counter}</button>
+    <div>
+      <HourNow />
+      <Button onClickFunction={incrementCounter} increment={1}/>
+      <Button onClickFunction={incrementCounter} increment={5}/>
+      <Button onClickFunction={incrementCounter} increment={10}/>
+      <Button onClickFunction={incrementCounter} increment={100}/>
+      <Display message={counter}/>
+    </div>
   );
 }
 
