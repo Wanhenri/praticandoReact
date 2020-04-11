2dc5b3607dc16508df348542c009e68b3a3b7d0c
Wanhenri <you@example.com>
2020-04-11 18:08:25 -0300

    [feat](frontend):Using Side Effects Hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,13 +10,65 @@ const StarsDisplay = props => (
 );
 
 const PlayNumber = props => (
-  <button  className="number" onClick={()=> console.log('Num', props.number)}>
+  <button  
+    className="number"
+    style={{ backgroundColor: colors[props.status]}} 
+    onClick={()=> props.onClick(props.number, props.status)}>
     {props.number}
   </button>
 )
 
+const PlayAgain = props => (
+  <div className="game-done">
+      <button onClick={props.onClick}>Play Again</button>
+  </div>
+);
+
 const App = () => {
- const [stars, setStars] = useState(utils.random(1, 9)); 
+ const [ stars, setStars] = useState(utils.random(1, 9)); 
+ const [ availableNums, setAvailableNums ] = useState(utils.range(1,9));
+ const [ candidateNums, setCandidateNums ] = useState([]);
+
+const candidateAreWrong = utils.sum(candidateNums) > stars;
+const gameIsDone = availableNums.length === 0;
+
+const resetGame = () => {
+  setStars(utils.random(1, 9));
+  setAvailableNums(utils.range(1, 9));
+  setCandidateNums([]);
+}
+
+ const numberStatus = (number) => {
+  if (!availableNums.includes(number)) {
+    return 'used';
+  }
+  if (candidateNums.includes(number)) {
+    return candidateAreWrong ? 'wrong' : 'candidate'
+  }
+  return 'available';
+ };
+
+ const onNumberClick = (number, currentStatus) => {
+   if (currentStatus == 'used') {
+     return;
+   }
+   const newCandidateNums = 
+    currentStatus === 'available'
+      ? candidateNums.concat(number)
+      : candidateNums.filter(cn => cn !== number);   
+   
+   if (utils.sum(newCandidateNums) !== stars){
+     setCandidateNums(newCandidateNums);
+   } else {
+     const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+     );
+     setStars(utils.randomSumIn(newAvailableNums, 9));
+     setAvailableNums(newAvailableNums);
+     setCandidateNums([]);
+   }
+ };
+
    return (
      <div className="game">
        <div className="help"> 
@@ -24,11 +76,21 @@ const App = () => {
        </div>
        <div className="body">
          <div className="left">
-           <StarsDisplay count={stars} />
+           {gameIsDone ? (
+             <PlayAgain onClick={resetGame}/>
+            ) : (
+            <StarsDisplay count={stars} 
+            />
+           )}
          </div >
            <div className="right">
              {utils.range(1,9).map(number =>
-               <PlayNumber key={number} number={number} />
+               <PlayNumber 
+                key={number} 
+                status={numberStatus(number)}
+                number={number} 
+                onClick={onNumberClick}
+              />
              )}
          </div>
        </div>
